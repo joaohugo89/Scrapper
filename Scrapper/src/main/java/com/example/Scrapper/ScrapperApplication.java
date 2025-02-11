6439566f22ed9f315a8de20e40e6fefaa8eb532c
joaohugo89 <joaohugosoares89@outlook.com>
2025-02-11 16:18:45 -0300

    first commit

---
+++ Scrapper/src/main/java/com/example/Scrapper/ScrapperApplication.java
@@ -0,0 +1,57 @@
+package com.example.Scrapper;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapperApplication {
+
+	public static void main(String[] args) {
+		try {
+			String baseUrl = "https://books.toscrape.com/";
+			String url = "https://books.toscrape.com/catalogue/";
+			Document baseDocument = Jsoup.connect(baseUrl).get();
+			Document document = Jsoup.connect(baseUrl).get();
+				Elements books = document.select(".product_pod");
+				System.out.println(baseUrl);
+				for (Element bk:books){
+					String title = bk.select("h3 > a").text();
+					String price = bk.select(".price_color").text();
+					System.out.println(title + " - " + price );
+				}
+			Elements nextElements = baseDocument.select(".next");
+			Element nextElement = nextElements.first();
+			String relativeUrl = nextElement.getElementsByTag("a").first().attr("href");
+
+			System.out.println("==================================================================");
+
+			while (!nextElements.isEmpty()) {
+				String completeUrl = baseUrl + relativeUrl;
+				document = Jsoup.connect(completeUrl).get();
+				books = document.select(".product_pod");
+				for (Element bk:books){
+					String title = bk.select("h3 > a").text();
+					String price = bk.select(".price_color").text();
+					System.out.println(title + " - " + price );
+				}
+				System.out.println("==================================================================");
+				baseUrl = url;
+				nextElements = document.select(".next");
+				nextElement = nextElements.first();
+				relativeUrl = nextElement.getElementsByTag("a").first().attr("href");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+	}
+}
+
---
+++ Scrapper/src/test/java/com/example/Scrapper/ScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
